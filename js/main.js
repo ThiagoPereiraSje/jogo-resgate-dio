5dd9bc6104df2c2404997cff89dcc300dc013524
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-16 13:52:55 -0300

    Movimentando o primeiro inimigo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+// Defini quais teclas do teclado serão utilizadas
 const TECLA = { W: 87, S: 83, D: 68 };
 const jogo = {
   pressionou: [],
 };
+
+// Velocidade e posição do inimigo 1
+const velocidade = 5;
+let posicaoY = randPosition();
 
 //Verifica se o usuário pressionou alguma tecla
 $(document).keydown(function (e) {
@@ -29,11 +34,16 @@ function start() {
 function loop() {
   moveFundo();
   moveJogador();
+  moveInimigo1();
 }
 
 function moveFundo() {
   esquerda = parseInt($("#fundoGame").css("background-position"));
   $("#fundoGame").css("background-position", esquerda - 1);
+}
+
+function randPosition() {
+  return parseInt(Math.random() * 324 + 10);
 }
 
 // função para mover o helicóptero cinza
@@ -58,5 +68,19 @@ function moveJogador() {
 
   if (jogo.pressionou[TECLA.D]) {
     // chama função disparo
+  }
+}
+
+// função para mover o inimigo 1, helicóptero amarelo
+function moveInimigo1() {
+  let posicaoX = parseInt($("#inimigo1").css("left"));
+
+  $("#inimigo1").css("left", posicaoX - velocidade);
+  $("#inimigo1").css("top", posicaoY);
+
+  if (posicaoX <= 0) {
+    posicaoY = randPosition();
+    $("#inimigo1").css("left", 694);
+    $("#inimigo1").css("top", posicaoY);
   }
 }
