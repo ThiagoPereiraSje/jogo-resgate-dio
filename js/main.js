46b83fdcdcccc24d2fdc5373416b028784f6aecb
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-17 10:38:15 -0300

    Verifica colisao com o inimigo1

--- js/main.js
+++ js/main.js
@@ -40,6 +40,7 @@ function loop() {
   moveInimigo1();
   moveInimigo2();
   moveAmigo();
+  colisao();
 }
 
 function moveFundo() {
@@ -141,4 +142,103 @@ function disparo() {
       podeAtirar = true;
     }
   }
+}
+
+// função da explosão, colisão com o inimigo 1, helicóptero
+function explosao1(inimigo1X, inimigo1Y) {
+  // somExplosao.play();
+
+  $("#fundoGame").append("<div id='explosao1'></div");
+
+  var div = $("#explosao1");
+  div.css("top", inimigo1Y);
+  div.css("left", inimigo1X);
+  div.animate({ width: 200, opacity: 0 }, "slow");
+
+  let tempoExplosao = window.setInterval(removeExplosao, 1000);
+
+  function removeExplosao() {
+    div.remove();
+    window.clearInterval(tempoExplosao);
+    tempoExplosao = null;
+  }
+}
+
+// função que verifica a colisão dos itens do jogo
+function colisao() {
+  const jogadorInimigo1 = $("#jogador").collision($("#inimigo1"));
+  // const jogadorInimigo2 = $("#jogador").collision($("#inimigo2"));
+  // const disparoInimigo1 = $("#disparo").collision($("#inimigo1"));
+  // const disparoInimigo2 = $("#disparo").collision($("#inimigo2"));
+  // const jogadorAmigo = $("#jogador").collision($("#amigo"));
+  // const amigoInimigo2 = $("#inimigo2").collision($("#amigo"));
+
+  // colisão do jogador (helicóptero) com o inimigo1, helicóptero
+  if (jogadorInimigo1.length > 0) {
+    // energiaAtual--;
+
+    inimigo1X = parseInt($("#inimigo1").css("left"));
+    inimigo1Y = parseInt($("#inimigo1").css("top"));
+    explosao1(inimigo1X, inimigo1Y);
+
+    posicaoY = parseInt(Math.random() * 334);
+    $("#inimigo1").css("left", 694);
+    $("#inimigo1").css("top", posicaoY);
+  }
+
+  // colisão do jogador (helicóptero) com o inimigo2, caminhão
+  // if (jogadorInimigo2.length > 0) {
+  //   energiaAtual--;
+  //   inimigo2X = parseInt($("#inimigo2").css("left"));
+  //   inimigo2Y = parseInt($("#inimigo2").css("top"));
+  //   explosao2(inimigo2X, inimigo2Y);
+
+  //   $("#inimigo2").remove();
+  //   reposicionaInimigo2();
+  // }
+
+  // colisão do disparo com o inimigo 1, helicóptero
+  // if (disparoInimigo1.length > 0) {
+  //   velocidade = velocidade + 0.3;
+  //   pontos = pontos + 100;
+  //   inimigo1X = parseInt($("#inimigo1").css("left"));
+  //   inimigo1Y = parseInt($("#inimigo1").css("top"));
+  //   explosao1(inimigo1X, inimigo1Y);
+
+  //   $("#disparo").css("left", 950);
+  //   posicaoY = parseInt(Math.random() * 334);
+  //   $("#inimigo1").css("left", 694);
+  //   $("#inimigo1").css("top", posicaoY);
+  // }
+
+  // colisão do disparo com o inimigo 2, caminhão
+  // if (disparoInimigo2.length > 0) {
+  //   pontos = pontos + 50;
+  //   inimigo2X = parseInt($("#inimigo2").css("left"));
+  //   inimigo2Y = parseInt($("#inimigo2").css("top"));
+  //   $("#inimigo2").remove();
+
+  //   explosao2(inimigo2X, inimigo2Y);
+  //   $("#disparo").css("left", 950);
+  //   reposicionaInimigo2();
+  // }
+
+  // colisão do jogador (helicóptero) com o amigo
+  // if (jogadorAmigo.length > 0) {
+  //   salvos++;
+  //   somResgate.play();
+  //   reposicionaAmigo();
+  //   $("#amigo").remove();
+  // }
+
+  // colisão do amigo com o inimigo 2, caminhão
+  // if (amigoInimigo2.length > 0) {
+  //   perdidos++;
+  //   amigoX = parseInt($("#amigo").css("left"));
+  //   amigoY = parseInt($("#amigo").css("top"));
+
+  //   explosao3(amigoX, amigoY);
+  //   $("#amigo").remove();
+  //   reposicionaAmigo();
+  // }
 }
